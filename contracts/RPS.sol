2f1886a2349fd55c313e52de6c69e2a3eb30f0d7
Katid Supawee <katidd17@gmail.com>
2024-02-08 13:17:12 +0700

    feat(commitment): add commit reveal and reset stage

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -5,38 +5,76 @@ pragma solidity >=0.7.0 <0.9.0;
 contract RPS {
   struct Player {
     uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
+    bytes32 commit;
     address addr;
   }
-  uint public numPlayer = 0;
   uint public reward = 0;
-  mapping(uint => Player) public player;
+  Player public player0;
+  Player public player1;
   uint public numInput = 0;
+  uint public numReveal = 0;
 
   function addPlayer() public payable {
-    require(numPlayer < 2);
+    require(player0.addr == address(0x0) || player1.addr == address(0x0));
     require(msg.value == 1 ether);
     reward += msg.value;
-    player[numPlayer].addr = msg.sender;
-    player[numPlayer].choice = 3;
-    numPlayer++;
+    if (player0.addr == address(0x0)) {
+      player0.addr = msg.sender;
+      player0.choice = 3;
+      player0.commit = 0;
+      return;
+    } else if (player1.addr == address(0x0)) {
+      player1.addr = msg.sender;
+      player1.choice = 3;
+      player1.commit = 0;
+      return;
+    }
   }
 
-  function input(uint choice, uint idx) public {
-    require(numPlayer == 2);
-    require(msg.sender == player[idx].addr);
-    require(choice == 0 || choice == 1 || choice == 2);
-    player[idx].choice = choice;
+  function input(bytes32 hashChoice) public {
+    require(player0.addr != address(0x0) && player1.addr != address(0x0));
+    require(numInput < 2);
+    if (msg.sender == player0.addr) {
+      require(player0.choice == 3); // not yet revealed
+      require(player0.commit == 0); // not yet committed
+      player0.commit = hashChoice;
+    } else if (msg.sender == player1.addr) {
+      require(player1.choice == 3); // not yet revealed
+      require(player1.commit == 0); // not yet committed
+      player1.commit = hashChoice;
+    }
     numInput++;
-    if (numInput == 2) {
+  }
+
+  function reveal(uint choice, string memory password) public {
+    require(numInput == 2);
+    require(numReveal < 2);
+    if (msg.sender == player0.addr) {
+      require(player0.choice == 3); // not yet revealed
+      require(player0.commit != 0); // already committed
+      require(
+        keccak256(abi.encodePacked(choice, password)) == player0.commit
+      );
+      player0.choice = choice;
+    } else if (msg.sender == player1.addr) {
+      require(player1.choice == 3); // not yet revealed
+      require(player1.commit != 0); // already committed
+      require(
+        keccak256(abi.encodePacked(choice, password)) == player1.commit
+      );
+      player1.choice = choice;
+    }
+    numReveal++;
+    if (numReveal == 2) {
       _checkWinnerAndPay();
     }
   }
 
   function _checkWinnerAndPay() private {
-    uint p0Choice = player[0].choice;
-    uint p1Choice = player[1].choice;
-    address payable account0 = payable(player[0].addr);
-    address payable account1 = payable(player[1].addr);
+    uint p0Choice = player0.choice;
+    uint p1Choice = player1.choice;
+    address payable account0 = payable(player0.addr);
+    address payable account1 = payable(player1.addr);
     if ((p0Choice + 1) % 3 == p1Choice) {
       // to pay player[1]
       account1.transfer(reward);
@@ -48,5 +86,27 @@ contract RPS {
       account0.transfer(reward / 2);
       account1.transfer(reward / 2);
     }
+    reward = 0;
+    _resetStage();
+  }
+
+  function _resetStage() private {
+    require(numReveal == 2);
+    player0.choice = 3;
+    player1.choice = 3;
+    player0.commit = 0;
+    player1.commit = 0;
+    player0.addr = address(0x0);
+    player1.addr = address(0x0);
+    numInput = 0;
+    numReveal = 0;
+  }
+
+  function getHash(uint choice, string memory password)
+    public
+    pure
+    returns (bytes32)
+  {
+    return keccak256(abi.encodePacked(choice, password));
   }
 }
