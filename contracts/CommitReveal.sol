d7c9d53693fa205e94e4de8236f9654326ddf255
Katid Supawee <katidd17@gmail.com>
2024-02-10 17:59:12 +0700

    refactor(commitReveal): remove unused function, change datatype and change to private

--- contracts/CommitReveal.sol
+++ contracts/CommitReveal.sol
@@ -11,9 +11,9 @@ contract CommitReveal {
     bool revealed;
   }
 
-  mapping(address => Commit) public commits;
+  mapping(address => Commit) internal commits;
 
-  function commit(bytes32 dataHash) public {
+  function commit(bytes32 dataHash) internal  {
     commits[msg.sender].commit = dataHash;
     commits[msg.sender].block = uint64(block.number);
     commits[msg.sender].revealed = false;
@@ -25,42 +25,9 @@ contract CommitReveal {
   }
   event CommitHash(address sender, bytes32 dataHash, uint64 block);
 
-  function reveal(bytes32 revealHash) public {
-    //make sure it hasn't been revealed yet and set it to revealed
-    require(
-      commits[msg.sender].revealed == false,
-      "CommitReveal::reveal: Already revealed"
-    );
-    commits[msg.sender].revealed = true;
-    //require that they can produce the committed hash
-    require(
-      getHash(revealHash) == commits[msg.sender].commit,
-      "CommitReveal::reveal: Revealed hash does not match commit"
-    );
-    //require that the block number is greater than the original block
-    require(
-      uint64(block.number) > commits[msg.sender].block,
-      "CommitReveal::reveal: Reveal and commit happened on the same block"
-    );
-    //require that no more than 250 blocks have passed
-    require(
-      uint64(block.number) <= commits[msg.sender].block + 250,
-      "CommitReveal::reveal: Revealed too late"
-    );
-    //get the hash of the block that happened after they committed
-    bytes32 blockHash = blockhash(commits[msg.sender].block);
-    //hash that with their reveal that so miner shouldn't know and mod it with some max number you want
-    uint random = uint(keccak256(abi.encodePacked(blockHash, revealHash))) %
-      max;
-    emit RevealHash(msg.sender, revealHash, random);
-  }
-  event RevealHash(address sender, bytes32 revealHash, uint random);
 
-  function getHash(bytes32 data) public view returns (bytes32) {
-    return keccak256(abi.encodePacked(address(this), data));
-  }
 
-  function revealAnswer(bytes32 answer, bytes32 salt) public {
+  function revealAnswer(uint answer, string memory salt) internal  {
     //make sure it hasn't been revealed yet and set it to revealed
     require(
       commits[msg.sender].revealed == false,
@@ -74,11 +41,11 @@ contract CommitReveal {
     );
     emit RevealAnswer(msg.sender, answer, salt);
   }
-  event RevealAnswer(address sender, bytes32 answer, bytes32 salt);
+  event RevealAnswer(address sender, uint  answer, string  salt);
 
   function getSaltedHash(
-    bytes32 data,
-    bytes32 salt
+    uint data,
+    string memory salt
   ) public view returns (bytes32) {
     return keccak256(abi.encodePacked(address(this), data, salt));
   }
