d24100f7a772a3d4a06a666862659718ce21cf68
Katid Supawee <katidd17@gmail.com>
2025-03-06 21:15:47 +0700

    Add allowedPlayerList and validation for player participation in RPS contract

--- RPS.sol
+++ RPS.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
@@ -6,9 +5,15 @@ pragma solidity >=0.7.0 <0.9.0;
 contract RPS {
     uint public numPlayer = 0;
     uint public reward = 0;
-    mapping (address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
+    mapping(address => uint) public player_choice; // 0 - Rock, 1 - Paper , 2 - Scissors
     mapping(address => bool) public player_not_played;
     address[] public players;
+    address[] public allowedPlayerList = [
+        0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
+        0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
+        0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,
+        0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+    ];
 
     uint public numInput = 0;
 
@@ -17,6 +22,15 @@ contract RPS {
         if (numPlayer > 0) {
             require(msg.sender != players[0]);
         }
+        // Player must be in allowedPlayerList
+        bool isAllowed = false;
+        for (uint i = 0; i < allowedPlayerList.length; i++) {
+            if (msg.sender == allowedPlayerList[i]) {
+                isAllowed = true;
+                break;
+            }
+        }
+        require(isAllowed);
         require(msg.value == 1 ether);
         reward += msg.value;
         player_not_played[msg.sender] = true;
@@ -24,7 +38,7 @@ contract RPS {
         numPlayer++;
     }
 
-    function input(uint choice) public  {
+    function input(uint choice) public {
         require(numPlayer == 2);
         require(player_not_played[msg.sender]);
         require(choice == 0 || choice == 1 || choice == 2);
@@ -44,12 +58,10 @@ contract RPS {
         if ((p0Choice + 1) % 3 == p1Choice) {
             // to pay player[1]
             account1.transfer(reward);
-        }
-        else if ((p1Choice + 1) % 3 == p0Choice) {
+        } else if ((p1Choice + 1) % 3 == p0Choice) {
             // to pay player[0]
-            account0.transfer(reward);    
-        }
-        else {
+            account0.transfer(reward);
+        } else {
             // to split reward
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
