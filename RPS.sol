424211fe501d7539b2acb1d86272c2a017d62e1b
Katid Supawee <katidd17@gmail.com>
2025-03-06 22:14:39 +0700

    Integrate TimeUnit contract into RPS; implement time-based game mechanics and withdrawal logic

--- RPS.sol
+++ RPS.sol
@@ -3,7 +3,8 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "./CommitReveal.sol";
-contract RPS is CommitReveal {
+import "./TimeUnit.sol";
+contract RPS is CommitReveal, TimeUnit {
     enum Choice {
         Scissors,
         Paper,
@@ -11,14 +12,7 @@ contract RPS is CommitReveal {
         Lizard,
         Spock
     }
-
-    uint public numPlayer = 0;
-    uint public numReveal = 0;
-    uint public reward = 0;
-    mapping(address => Choice) public player_choice;
-    mapping(address => bytes32) public player_hashedChoice;
-    mapping(address => bool) public player_not_played;
-    address[] public players;
+    uint public constant TIME_LIMIT = 1 minutes;
     address[] public allowedPlayerList = [
         0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
         0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,
@@ -26,7 +20,14 @@ contract RPS is CommitReveal {
         0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
     ];
 
+    uint public numPlayer = 0;
     uint public numInput = 0;
+    uint public numReveal = 0;
+    uint public reward = 0;
+    mapping(address => Choice) public player_choice;
+    mapping(address => bytes32) public player_hashedChoice;
+    mapping(address => bool) public player_not_played;
+    address[] public players;
 
     function addPlayer() public payable {
         require(numPlayer < 2);
@@ -42,6 +43,13 @@ contract RPS is CommitReveal {
             }
         }
         require(isAllowed);
+
+        if (numPlayer == 0) {
+            _setStartTime();
+        } else if (numPlayer > 0) {
+            require(elapsedMinutes() < TIME_LIMIT);
+        }
+
         require(msg.value == 1 ether);
         reward += msg.value;
         player_not_played[msg.sender] = true;
@@ -59,6 +67,7 @@ contract RPS is CommitReveal {
     function inputHashedChoice(bytes32 hashedChoice) public {
         require(numPlayer == 2);
         require(player_not_played[msg.sender]);
+        require(elapsedSeconds() < TIME_LIMIT);
 
         commit(hashedChoice);
         player_hashedChoice[msg.sender] = hashedChoice;
@@ -77,6 +86,7 @@ contract RPS is CommitReveal {
                 choice == Choice.Lizard ||
                 choice == Choice.Spock
         );
+        require(elapsedSeconds() < TIME_LIMIT);
 
         reveal(bytes32(abi.encodePacked(choice, secret)));
         player_choice[msg.sender] = choice;
@@ -112,6 +122,49 @@ contract RPS is CommitReveal {
         }
     }
 
+    function withdraw() public {
+        require(elapsedSeconds() >= TIME_LIMIT);
+        require(numPlayer > 0);
+
+        // Case 1: Only 1 player join
+        if (numPlayer == 1) {
+            address payable account0 = payable(players[0]);
+            account0.transfer(reward);
+        }
+        // Case 2: 2 players join
+        else if (numPlayer == 2) {
+            address payable account0 = payable(players[0]);
+            address payable account1 = payable(players[1]);
+
+            // Case not play
+            if (
+                player_not_played[players[0]] && player_not_played[players[1]]
+            ) {
+                // split reward if both player not play
+                account0.transfer(reward / 2);
+                account1.transfer(reward / 2);
+            } else if (player_not_played[players[0]]) {
+                account1.transfer(reward);
+            } else if (player_not_played[players[1]]) {
+                account0.transfer(reward);
+            }
+            // Case not reveal
+            else if (
+                commits[players[0]].revealed && commits[players[1]].revealed
+            ) {
+                // split reward if both player not reveal
+                account0.transfer(reward / 2);
+                account1.transfer(reward / 2);
+            } else if (commits[players[0]].revealed) {
+                account1.transfer(reward);
+            } else if (commits[players[1]].revealed) {
+                account0.transfer(reward);
+            }
+        }
+
+        _resetGame();
+    }
+
     function _resetGame() private {
         numPlayer = 0;
         numReveal = 0;
@@ -121,5 +174,15 @@ contract RPS is CommitReveal {
         for (uint i = 0; i < allowedPlayerList.length; i++) {
             player_not_played[allowedPlayerList[i]] = false;
         }
+    }
+
+    function secondsUntilWithdrawableTime() public view returns (uint) {
+        if (numPlayer == 0) {
+            return 0;
+        }
+        if (elapsedSeconds() >= TIME_LIMIT) {
+            return 0;
+        }
+        return TIME_LIMIT - elapsedSeconds();
     }
 }
